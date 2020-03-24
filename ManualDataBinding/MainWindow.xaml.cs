c9e1b7ebec90475e4f7b0ad4c41498bbc8f1a981
Mark Christiansen <mfchrist@ksu.edu>
2020-03-23 22:01:39 -0500

    Program working correctly

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,47 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Create the note to add to our editor
+        /// </summary>
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Event handler to create a new note and apply it to the editor.
+        /// </summary>
+        /// <param name="sender">button clicked</param>
+        /// <param name="e">event args</param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Event handler to clear contents of note.
+        /// </summary>
+        /// <param name="sender">button clicked</param>
+        /// <param name="e">event args</param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+
+        /// <summary>
+        /// Event handler to mutate current note
+        /// </summary>
+        /// <param name="sender">button clicked</param>
+        /// <param name="e">event args</param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Jayden Smith";
+            note.Body = "How can mirrors be real if our eyes arent real?";
         }
     }
 }
